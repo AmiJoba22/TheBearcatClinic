7bcd3a628eee237be35cfd6fe2b2fe309505c463
AmiJoba657 <axj342@student.bham.ac.uk>
2025-04-09 17:50:16 -0400

    Login + register

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,48 @@
 import React from 'react'
-
+import "../styles/RegisterStyles.css";
+import { Form, Input } from "antd";
+import { Link } from "react-router-dom";
 const Login = () => {
+
+   // Form handler
+   const onFinishHandler = (values) => {
+    console.log(values);
+  };
+  //Login Form
   return (
-    <div>
-      <h1>
+    <div className="form-container">
+    <Form
+      layout="vertical"
+      onFinish={onFinishHandler}
+      className="register-form"
+    >
+      <h3 className="text-center">Login Form</h3>
+
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: "Please enter your email" }]}
+      >
+        <Input type="email" />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please enter your password" }]}
+      >
+        <Input type="password" />
+      </Form.Item>
+
+      <div style={{ marginBottom: "1rem" }}>
+        <Link to="/register">Not a User? Register here</Link>
+      </div>
+
+      <button className="btn btn-primary" type="submit">
         Login
-      </h1>
-    </div>
+      </button>
+    </Form>
+  </div>
   )
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,60 @@
-import React from 'react'
+import React from "react";
+import "../styles/RegisterStyles.css";
+import { Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  
+  // Form handler
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
+  // Register Form 
   return (
-    <div>
-      <h1>
-        Register Page
-      </h1>
-    </div>
-  )
-}
+    <>
+      <div className="form-container">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="register-form"
+        >
+          <h3 className="text-center">Register Form</h3>
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please enter your name" }]}
+          >
+            <Input type="text" />
+          </Form.Item>
 
-export default Register
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please enter your email" }]}
+          >
+            <Input type="email" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please enter your password" }]}
+          >
+            <Input type="password" />
+          </Form.Item>
+
+          <div style={{ marginBottom: "1rem" }}>
+            <Link to="/login">Already a user? Login here</Link>
+          </div>
+
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
+
